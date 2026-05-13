118caae9813c41f86061681f2eca7c2a4bc72481
Nguyen Tri Nang <nangnt0575@ut.edu.vn>
2026-05-13 17:28:22 +0700

    Add Chapter Entity

--- src/main/java/com/webtruyen/first/entity/Chapter.java
+++ src/main/java/com/webtruyen/first/entity/Chapter.java
@@ -1,5 +1,59 @@
 package com.webtruyen.first.entity;
 
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.webtruyen.first.enums.ChapterStatus;
+import com.webtruyen.first.enums.StoryStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chapters")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Chapter {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private int chapterNumber;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private ChapterStatus status = ChapterStatus.PUBLISHED;
+
+    @ManyToOne
+    @JoinColumn(name = "story_id")
+    private Story story;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
 }
