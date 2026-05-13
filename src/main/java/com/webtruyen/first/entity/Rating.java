5a7c7bd347ad69f560d2f20b6bb1c434ffa2677b
Nguyen Tri Nang <nangnt0575@ut.edu.vn>
2026-05-13 18:28:10 +0700

    Add Rating Entity

---
+++ src/main/java/com/webtruyen/first/entity/Rating.java
@@ -0,0 +1,40 @@
+package com.webtruyen.first.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ratings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "story_id")
+    private Story story;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(nullable = false)
+    private int score; // 1-5
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+}
